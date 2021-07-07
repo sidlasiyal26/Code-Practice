f88d3afea0212c3f03f3d6a86e1a143d37f34eaa
sidlasiyal26 <65032091+sidlasiyal26@users.noreply.github.com>
2021-07-07 14:57:59 +0530

    Update Lucky Numbers in a Matrix.cpp

--- Day 7/Lucky Numbers in a Matrix.cpp
+++ Day 7/Lucky Numbers in a Matrix.cpp
@@ -3,36 +3,25 @@ public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int r[m],c[n];
-        for(int i=0;i<m;i++)
-        {int min=100001;
-         for(int j=0;j<n;j++)
-         {
-           if(min>matrix[i][j])
-            min=matrix[i][j];
-           }r[i]=min;
-        }
+         vector<int>a;
+        int pos;
+       
       for(int j=0;j<n;j++)
        {
           int max=-1;
       for(int i=0;i<m;i++)
        {
           if(max<matrix[i][j])
-          max=matrix[i][j];
+          {max=matrix[i][j];
+          pos=i;
+          }
        }  
-        c[j]=max;
+        
+          if(max==*min_element(matrix[pos].begin(),matrix[pos].end()))
+            a.push_back(max);   
                                                                     
          }
-        vector<int>a;
-       for(int i=0;i<m;i++)
-     {
-      for(int j=0;j<n;j++)
-    {
-          if(r[i]==c[j])
-        a.push_back(matrix[i][j]);
-      }
-       
-     }
+     
         return a;
                                                             
         
